466de6ea2cb83e41729ba8b0950e65b005bf9135
kindybigand <56339956+kindybigand@users.noreply.github.com>
2020-04-10 08:33:54 +0200

    Update classification.m

--- code/classification.m
+++ code/classification.m
@@ -1,20 +1,28 @@
-function [classes] = classification(caracteristiques)
+function [classes] = classification(caracteristiques,dimensionimage)
 
     for k = 1:length(caracteristiques)
         
         classes(k).x = caracteristiques(k).x;
         classes(k).y = caracteristiques(k).y;
         
+        coefperspective=dimensionimage(2)-caracteristiques(k).y/dimensionimage(2);
         if abs(caracteristiques(k).teinteMoyenne - 0.3333) < 0.1
             classes(k).couleur = 'vert';
         else
             classes(k).couleur = 'rouge';
         end
         
-        classes(k).position = '/\';
+        if abs(caracteristiques(k).x - 3) < 15
+            classes(k).position = '/\';
+        else if abs(caracteristiques(k).x - 3) > 50
+                classes(k).position = '\/';
+            else
+                classes(k).position = '--';
+            end
+        end
+            
+            
+         
     end
-    
-    
-    
 end
 
