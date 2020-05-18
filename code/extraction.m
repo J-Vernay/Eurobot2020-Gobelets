c4cef12094296dcd92cbea4317146463a13b97a2
Matthieu-Poulain <Matthieu.poulain@telecom-st-etienne.fr>
2020-05-18 15:50:42 +0200

    Update extraction.m

--- code/extraction.m
+++ code/extraction.m
@@ -2,7 +2,7 @@ function [Gobelets] = extraction(imagePretraitee,espaceUtilise)
 % exemple de sortie
 % il doit y avoir autant de "caracteristiques(N)" que de gobelets
 % détectés
-figure; subplot(1,2,1); imshow(imagePretraitee,[]);
+% figure; subplot(1,2,1); imshow(imagePretraitee,[]);
 img=rgb2gray(imagePretraitee);
 moyenne=median(median(img));
 for i=1:size(img,1)
@@ -21,7 +21,7 @@ contourext=contourext-img;
 lpe=imfill(contourext,'holes');
 lpe=tse_imsplitobjects(logical(lpe));
 lpe=imopen(lpe, strel('disk',4));
-subplot(1,2,2);imshow(lpe,[]);
+% subplot(1,2,2);imshow(lpe,[]);
 
 f = lpe .* imagePretraitee;
 
@@ -33,7 +33,7 @@ f = lpe .* imagePretraitee;
 
 %% Classification et recupération des valeurs des pixels
 [fs,centers]=tse_imkmeans(f,3,0.01);
-figure(5); imshow(fs,[]);
+% figure(5); imshow(fs,[]);
 if (espaceUtilise == "rgb")
     Iclasse1 = double(fs==1) .* (f); % Fond
     Iclasse2 = double(fs==2) .* (f);
@@ -50,7 +50,15 @@ elseif (espaceUtilise == "hsv")
 %     Translation de H pour eviter la séparation du rouge
     for i = 1:l
         for j = 1:c
-            if (f(i,j,1)==0 && f(i,j,2)==0 && f(i,j,3)==0 )
+            if fs(i,j)==1
+                Iclasse1(i,j,:) = [NaN,NaN,NaN];
+                Iclasse2(i,j,:) = [NaN,NaN,NaN];
+                Iclasse3(i,j,:) = [NaN,NaN,NaN];
+            elseif fs(i,j)==2
+                Iclasse3(i,j,:) = [NaN,NaN,NaN];
+            elseif fs(i,j)==3
+                Iclasse2(i,j,:) = [NaN,NaN,NaN];
+            elseif (f(i,j,1)==0 && f(i,j,2)==0 && f(i,j,3)==0 )
                 Iclasse1(i,j,:) = [NaN,NaN,NaN];
                 Iclasse2(i,j,:) = [NaN,NaN,NaN];
                 Iclasse3(i,j,:) = [NaN,NaN,NaN];
@@ -70,19 +78,33 @@ elseif (espaceUtilise == "hsv")
         end
     end
     
-    moyParCanalClasse2 = [ mean(mean(Iclasse2(:,:,1),"omitnan"),"omitnan"), mean(mean(Iclasse2(:,:,2),"omitnan"),"omitnan"), mean(mean(Iclasse2(:,:,3),"omitnan"),"omitnan") ];
-moyParCanalClasse3 = [  mean(mean(Iclasse3(:,:,1),"omitnan"),"omitnan"), mean(mean(Iclasse3(:,:,2),"omitnan"),"omitnan"), mean(mean(Iclasse3(:,:,3),"omitnan"),"omitnan") ];
-EcartTypeParCanalClasse2 = [  std(std(Iclasse2(:,:,1),"omitnan"),"omitnan"), std(std(Iclasse2(:,:,2),"omitnan"),"omitnan"), std(std(Iclasse2(:,:,3),"omitnan"),"omitnan") ];
-EcartTypeParCanalClasse3 = [ std(std(Iclasse3(:,:,1),"omitnan"),"omitnan"), std(std(Iclasse3(:,:,2),"omitnan"),"omitnan"), std(std(Iclasse3(:,:,3),"omitnan"),"omitnan") ];
+    pixelsClasse2 = reshape(Iclasse2, [], 3);
+    pixelsClasse3 = reshape(Iclasse3, [], 3);
+    %     moyParCanalClasse2 = [ mean(mean(Iclasse2(:,:,1),"omitnan"),"omitnan"), mean(mean(Iclasse2(:,:,2),"omitnan"),"omitnan"), mean(mean(Iclasse2(:,:,3),"omitnan"),"omitnan") ];
+    moyParCanalClasse2 = mean(pixelsClasse2(~isnan(pixelsClasse2(:, 1)),:));
+    moyParCanalClasse3 = mean(pixelsClasse3(~isnan(pixelsClasse3(:, 1)),:));
+    %     moyParCanalClasse3 = [  mean(mean(Iclasse3(:,:,1),"omitnan"),"omitnan"), mean(mean(Iclasse3(:,:,2),"omitnan"),"omitnan"), mean(mean(Iclasse3(:,:,3),"omitnan"),"omitnan") ];
+    
+    EcartTypeParCanalClasse2 = std(pixelsClasse2(~isnan(pixelsClasse2(:, 1)),:));
+    EcartTypeParCanalClasse3 = std(pixelsClasse3(~isnan(pixelsClasse3(:, 1)),:));
 
 elseif (espaceUtilise == "lab")
     Iclasse1 = (double(fs==1) .* rgb2lab(f)); % Fond
-    Iclasse2 = (double(fs==2) .* rgb2lab(f));
+    Iclasse2 = (logical(fs==2) .* rgb2lab(f));
     Iclasse3 = (double(fs==3) .* rgb2lab(f));
     
     for i = 1:l
         for j = 1:c
-            if (f(i,j,1)==0 && f(i,j,2)==0 && f(i,j,3)==0 )
+            if fs(i,j)==1
+                Iclasse1(i,j,:) = [NaN,NaN,NaN];
+                Iclasse2(i,j,:) = [NaN,NaN,NaN];
+                Iclasse3(i,j,:) = [NaN,NaN,NaN];
+            elseif fs(i,j)==2
+                Iclasse3(i,j,:) = [NaN,NaN,NaN];
+            elseif fs(i,j)==3
+                Iclasse2(i,j,:) = [NaN,NaN,NaN];
+                
+            elseif(f(i,j,1)==0 && f(i,j,2)==0 && f(i,j,3)==0 )
                 Iclasse1(i,j,:) = [NaN,NaN,NaN];
                 Iclasse2(i,j,:) = [NaN,NaN,NaN];
                 Iclasse3(i,j,:) = [NaN,NaN,NaN];
@@ -90,11 +112,15 @@ elseif (espaceUtilise == "lab")
         end
     end
     
+    pixelsClasse2 = reshape(Iclasse2, [], 3);
+    pixelsClasse3 = reshape(Iclasse3, [], 3);
+    %     moyParCanalClasse2 = [ mean(mean(Iclasse2(:,:,1),"omitnan"),"omitnan"), mean(mean(Iclasse2(:,:,2),"omitnan"),"omitnan"), mean(mean(Iclasse2(:,:,3),"omitnan"),"omitnan") ];
+    moyParCanalClasse2 = mean(pixelsClasse2(~isnan(pixelsClasse2(:, 1)),:));
+    moyParCanalClasse3 = mean(pixelsClasse3(~isnan(pixelsClasse3(:, 1)),:));
+    %     moyParCanalClasse3 = [  mean(mean(Iclasse3(:,:,1),"omitnan"),"omitnan"), mean(mean(Iclasse3(:,:,2),"omitnan"),"omitnan"), mean(mean(Iclasse3(:,:,3),"omitnan"),"omitnan") ];
     
-    moyParCanalClasse2 = [ mean(mean(Iclasse2(:,:,1),"omitnan"),"omitnan"), mean(mean(Iclasse2(:,:,2),"omitnan"),"omitnan"), mean(mean(Iclasse2(:,:,3),"omitnan"),"omitnan") ];
-moyParCanalClasse3 = [  mean(mean(Iclasse3(:,:,1),"omitnan"),"omitnan"), mean(mean(Iclasse3(:,:,2),"omitnan"),"omitnan"), mean(mean(Iclasse3(:,:,3),"omitnan"),"omitnan") ];
-EcartTypeParCanalClasse2 = [  std(std(Iclasse2(:,:,1),"omitnan"),"omitnan"), std(std(Iclasse2(:,:,2),"omitnan"),"omitnan"), std(std(Iclasse2(:,:,3),"omitnan"),"omitnan") ];
-EcartTypeParCanalClasse3 = [ std(std(Iclasse3(:,:,1),"omitnan"),"omitnan"), std(std(Iclasse3(:,:,2),"omitnan"),"omitnan"), std(std(Iclasse3(:,:,3),"omitnan"),"omitnan") ];
+    EcartTypeParCanalClasse2 = std(pixelsClasse2(~isnan(pixelsClasse2(:, 1)),:));
+    EcartTypeParCanalClasse3 = std(pixelsClasse3(~isnan(pixelsClasse3(:, 1)),:));
 
 
 
@@ -128,7 +154,7 @@ if (espaceUtilise == 'rgb')
             end
         end
     end
-    figure; imshow(f3,[]);
+%     figure; imshow(f3,[]);
     
     
     %HSV
@@ -161,7 +187,7 @@ elseif (espaceUtilise == 'hsv')
         end
     end
     f4 = hsv2rgb(f3);
-    figure; imshow(f4,[]);
+%     figure; imshow(f4,[]);
     
     %Lab
 elseif (espaceUtilise == 'lab')
@@ -170,17 +196,17 @@ elseif (espaceUtilise == 'lab')
     f3 = f2;
     for i = 1:l
         for j = 1:c
-            if (f2(i,j,2)<-20 && f2(i,j,1)>10)
-                f3(i,j,:)=[60,-50,0];
-            elseif (f2(i,j,2)>30 && f2(i,j,1)>10)
+            if (  (f2(i,j,2)<86 && f2(i,j,2)>14) && (f2(i,j,3)<77 && f2(i,j,3)>-13) )
                 f3(i,j,:)=[60,70,60];
+            elseif ((f2(i,j,2)<0 && f2(i,j,2)>-60) && (f2(i,j,3)<30 && f2(i,j,3)>-18))
+                f3(i,j,:)=[60,-50,0];
             else
                 f3(i,j,:) = [NaN,NaN,NaN];
             end
         end
     end
     f4 = lab2rgb(f3);
-    figure; imshow(f,[]);
+%     figure; imshow(f4,[]);
 end
 
 
@@ -188,15 +214,15 @@ end
 %% Extraction des autres caracteristiques
 
 FBW = im2bw(f,0.145);
-figure(2)
-imshow(FBW);
+% figure(2)
+% imshow(FBW);
 s = strel('disk',1);
 fbin3 = imopen(FBW,s);
 
 
 fLabel=bwlabel(fbin3,4);
-figure(3);
-imshow(fLabel,[]);colorbar; colormap jet;
+% figure(3);
+% imshow(fLabel,[]);colorbar; colormap jet;
 Gobelets = struct("Circularite",0,"Aire",0,"Barycentre",0,"Orientation",0,"Perimetre",0,"couleur", "fond");
 
 for i = (1:max(max(fLabel)))
