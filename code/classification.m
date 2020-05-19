06a9111f31c8237113947311762fd1ca78080eeb
kindybigand <56339956+kindybigand@users.noreply.github.com>
2020-05-19 22:19:57 +0200

    dernieres modifications ( annotations + correction permiere image resultante)

--- code/classification.m
+++ code/classification.m
@@ -1,4 +1,10 @@
 function [classes] = classification(caracteristiques,dimensionimage)
+    % création des vecteurs contenant les caractéristiques des gobelets
+    % expérimentaux
+    % Entrée: 
+    % Sortie: image avec caractéristiques inscrites sur les barycentres
+    
+
     j=0;
     for k = 1:length(caracteristiques)
         
@@ -6,9 +12,9 @@ function [classes] = classification(caracteristiques,dimensionimage)
             j=j+1;
             classes(j).couleur = caracteristiques(k).couleur;
             classes(j).Barycentre=caracteristiques(k).Barycentre;
-        
-        % 384 49 point d'intersection?
-        
+            figure('visible', 'off');
+            %création de la droite reliant le point d'intersection décidé
+            %manuellement (35,384) et le barycentre des objets
             hold on,
             line([caracteristiques(k).Barycentre(1) 384],[caracteristiques(k).Barycentre(2) 35]);
             angle=atand((35-caracteristiques(k).Barycentre(2))/(caracteristiques(k).Barycentre(1)-384));
@@ -18,14 +24,18 @@ function [classes] = classification(caracteristiques,dimensionimage)
             y=round(caracteristiques(k).Barycentre(2)+100*cosd(caracteristiques(k).Orientation+90));
             hold on,
             line([caracteristiques(k).Barycentre(1) x],[caracteristiques(k).Barycentre(2) y], 'Color','red');
-            %a=caracteristiques(k).Orientation;
+            
+            %comparaison des angles entre les deux lignes tracées pour
+            %décider debout ou couché
             resultat(k)=caracteristiques(k).Orientation-angle;
-            if resultat(k) < 5 && resultat(k) > -5
+            if resultat(k) < 5 && resultat(k) > -5 % marge de 5 degrés
                 classes(j).position = "debout";
             else
                 classes(j).position = "couche";
             end      
         end
+        
+     close(figure);
     
         
 end
